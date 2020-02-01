fd06d2451b9537c2531d6ad239c4c7d894630c0a
Gábor Boros <gabor.brs@gmail.com>
2020-02-01 12:06:26 +0100

    Add documentation and unit tests for operations

--- hammurabi/rules/operations.py
+++ hammurabi/rules/operations.py
@@ -1,3 +1,8 @@
+"""
+Operations module contains common file/directory operation which can be
+handy when need to move, rename or copy files.
+"""
+
 import logging
 from pathlib import Path
 import shutil
@@ -8,7 +13,29 @@ from hammurabi.rules.common import SinglePathRule
 
 class Moved(SinglePathRule):
     """
-    TODO:
+    Move a file or directory from "A" to "B".
+
+    Example usage:
+
+    .. code-block:: python
+
+            >>> from pathlib import Path
+            >>> from hammurabi import Law, Pillar, Moved
+            >>>
+            >>> example_law = Law(
+            >>>     name="Name of the law",
+            >>>     description="Well detailed description what this law does.",
+            >>>     rules=(
+            >>>         Moved(
+            >>>             name="Move pyproject.toml to its place",
+            >>>             path=Path("/tmp/generated/pyproject.toml.template"),
+            >>>             destination=Path("./pyproject.toml"),  # Notice the rename!
+            >>>         ),
+            >>>     )
+            >>> )
+            >>>
+            >>> pillar = Pillar()
+            >>> pillar.register(example_law)
     """
 
     def __init__(
@@ -18,10 +45,6 @@ class Moved(SinglePathRule):
         destination: Optional[Path] = None,
         **kwargs,
     ):
-        """
-        TODO: Fill this
-        """
-
         self.destination = self.validate(destination, required=True)
         super().__init__(name, path, **kwargs)
 
@@ -35,7 +58,12 @@ class Moved(SinglePathRule):
 
     def task(self) -> Path:
         """
-        Move the given path to the destination
+        Move the given path to the destination. In case the file got a
+        new name when destination is provided, the file/directory will
+        be moved to its new place with its new name.
+
+        :return: Returns the new destination of the file/directory
+        :rtype: Path
         """
 
         logging.debug('Moving "%s" to "%s"', str(self.param), str(self.destination))
@@ -46,7 +74,30 @@ class Moved(SinglePathRule):
 
 class Renamed(Moved):
     """
-    Ensure that the given file or directory is renamed.
+    This rule is a shortcut for :class:`hammurabi.rules.operations.Moved`.
+    Instead of destination path a new name is required.
+
+    Example usage:
+
+    .. code-block:: python
+
+            >>> from pathlib import Path
+            >>> from hammurabi import Law, Pillar, Renamed
+            >>>
+            >>> example_law = Law(
+            >>>     name="Name of the law",
+            >>>     description="Well detailed description what this law does.",
+            >>>     rules=(
+            >>>         Renamed(
+            >>>             name="Rename pyproject.toml.bkp",
+            >>>             path=Path("/tmp/generated/pyproject.toml.bkp"),
+            >>>             new_name="pyproject.toml",
+            >>>         ),
+            >>>     )
+            >>> )
+            >>>
+            >>> pillar = Pillar()
+            >>> pillar.register(example_law)
     """
 
     def __init__(
@@ -56,10 +107,6 @@ class Renamed(Moved):
         new_name: Optional[str] = None,
         **kwargs,
     ):
-        """
-        TODO: Fill this
-        """
-
         path_name: str = self.validate(new_name, required=True)
         destination = Path((path or self.param).parent, path_name)
         super().__init__(name, path, destination, **kwargs)
@@ -77,10 +124,6 @@ class Copied(SinglePathRule):
         destination: Optional[Path] = None,
         **kwargs,
     ):
-        """
-        TODO: Fill this
-        """
-
         self.destination = self.validate(destination, required=True)
         super().__init__(name, path, **kwargs)
 
@@ -94,9 +137,16 @@ class Copied(SinglePathRule):
     def task(self) -> Path:
         """
         Copy the given file or directory to a new place.
+
+        :return: Returns the path of the copied file/directory
+        :rtype: Path
         """
 
         logging.debug('Copying "%s" to "%s"', str(self.param), str(self.destination))
-        shutil.copytree(self.param, self.destination)
+
+        if self.param.is_dir():
+            shutil.copytree(self.param, self.destination)
+        else:
+            shutil.copy2(self.param, self.destination)
 
         return self.destination
---
+++ tests/rules/test_operations.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+
+from mock import Mock, patch
+
+from hammurabi.rules.operations import Copied, Moved, Renamed
+
+
+@patch("hammurabi.rules.operations.shutil")
+def test_moved(mocked_shutil):
+    source = Mock()
+    destination = Mock()
+
+    rule = Moved(name="Moved rule", path=source, destination=destination)
+    rule.git_add = Mock()
+    rule.git_remove = Mock()
+
+    result = rule.task()
+    rule.post_task_hook()
+
+    assert result == destination
+    mocked_shutil.move.assert_called_once_with(source, destination)
+    rule.git_remove.assert_called_once_with(source)
+    rule.git_add.assert_called_once_with(destination)
+
+
+@patch("hammurabi.rules.operations.shutil")
+def test_renamed(mocked_shutil):
+    source = Path("/tmp/apple/tree")
+    new_name = "tree2"
+    destination = Path("/tmp/apple/", new_name)
+
+    rule = Renamed(name="Renamed rule", path=source, new_name=new_name)
+    rule.git_add = Mock()
+    rule.git_remove = Mock()
+
+    result = rule.task()
+    rule.post_task_hook()
+
+    assert result == destination
+    mocked_shutil.move.assert_called_once_with(source, destination)
+    rule.git_remove.assert_called_once_with(source)
+    rule.git_add.assert_called_once_with(destination)
+
+
+@patch("hammurabi.rules.operations.shutil")
+def test_file_copied(mocked_shutil):
+    source = Mock()
+    destination = Mock()
+    source.is_dir.return_value = False
+
+    rule = Copied(name="Copied rule", path=source, destination=destination)
+    rule.git_add = Mock()
+
+    result = rule.task()
+    rule.post_task_hook()
+
+    assert result == destination
+    mocked_shutil.copy2.assert_called_once_with(source, destination)
+    rule.git_add.assert_called_once_with(destination)
+
+
+@patch("hammurabi.rules.operations.shutil")
+def test_directory_copied(mocked_shutil):
+    source = Mock()
+    destination = Mock()
+    source.is_dir.return_value = True
+
+    rule = Copied(name="Copied rule", path=source, destination=destination)
+    rule.git_add = Mock()
+
+    result = rule.task()
+    rule.post_task_hook()
+
+    assert result == destination
+    mocked_shutil.copytree.assert_called_once_with(source, destination)
+    rule.git_add.assert_called_once_with(destination)
