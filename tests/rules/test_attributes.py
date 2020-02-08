1b54be2ccdfc2883a6dde72e3d6ed3b2f92d14eb
Gábor Boros <gabor.brs@gmail.com>
2020-02-08 12:22:52 +0100

    Rename expected_param to expected_path

--- tests/rules/test_attributes.py
+++ tests/rules/test_attributes.py
@@ -20,86 +20,86 @@ def test_single_attribute_rule():
     class is that the post_task hook does what we expect.
     """
 
-    expected_param = Path("test/path")
+    expected_path = Path("test/path")
 
     rule = ExampleSingleAttributeRule(
-        name="Single attribute base rule", path=expected_param, new_value="test-val"
+        name="Single attribute base rule", path=expected_path, new_value="test-val"
     )
 
     rule.git_add = Mock()
     rule.post_task_hook()
 
-    rule.git_add.assert_called_once_with(expected_param)
+    rule.git_add.assert_called_once_with(expected_path)
 
 
 @given(user=st.text(min_size=2))
 @patch("hammurabi.rules.attributes.shutil")
 def test_owner_changed(mocked_shutil, user):
-    expected_param = Path("test/path")
+    expected_path = Path("test/path")
     expected_user = user.strip() or None
     expected_group = None
 
     rule = OwnerChanged(
-        name="Change owner of file", path=expected_param, new_value=user
+        name="Change owner of file", path=expected_path, new_value=user
     )
 
     result = rule.task()
 
-    assert result == expected_param
+    assert result == expected_path
     mocked_shutil.chown.assert_called_once_with(
-        str(expected_param), user=expected_user, group=expected_group
+        str(expected_path), user=expected_user, group=expected_group
     )
 
 
 @given(group=st.text(min_size=1))
 @patch("hammurabi.rules.attributes.shutil")
 def test_group_changed(mocked_shutil, group):
-    expected_param = Path("test/path")
+    expected_path = Path("test/path")
     expected_user = None
     expected_group = group.strip() or None
 
     rule = OwnerChanged(
-        name="Change owner of file", path=expected_param, new_value=f":{group}"
+        name="Change owner of file", path=expected_path, new_value=f":{group}"
     )
 
     result = rule.task()
 
-    assert result == expected_param
+    assert result == expected_path
     mocked_shutil.chown.assert_called_once_with(
-        str(expected_param), user=expected_user, group=expected_group
+        str(expected_path), user=expected_user, group=expected_group
     )
 
 
 @patch("hammurabi.rules.attributes.shutil")
 def test_owner_and_group_changed(mocked_shutil):
-    expected_param = Path("test/path")
+    expected_path = Path("test/path")
     expected_user = "user"
     expected_group = "group"
 
     rule = OwnerChanged(
         name="Change owner of file",
-        path=expected_param,
+        path=expected_path,
         new_value=f"{expected_user}:{expected_group}",
     )
 
     result = rule.task()
 
-    assert result == expected_param
+    assert result == expected_path
     mocked_shutil.chown.assert_called_once_with(
-        str(expected_param), user=expected_user, group=expected_group
+        str(expected_path), user=expected_user, group=expected_group
     )
 
 
 @patch("hammurabi.rules.attributes.os")
 def test_mode_changed(mocked_os):
-    expected_param = Path("test/path")
+    expected_path = Path("test/path")
     expected_mode = stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
 
     rule = ModeChanged(
-        name="Change owner of file", path=expected_param, new_value=expected_mode
+        name="Change owner of file", path=expected_path, new_value=expected_mode
     )
 
     result = rule.task()
 
-    assert result == expected_param
-    mocked_os.chmod.assert_called_once_with(str(expected_param), expected_mode)
+    assert result == expected_path
+    mocked_os.chmod.assert_called_once_with(str(expected_path), expected_mode)
